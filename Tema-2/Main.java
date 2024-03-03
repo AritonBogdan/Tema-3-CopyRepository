084c9493a06ef74bfae2524c15787f689eabb3f8
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-03 15:12:00 +0200

    Copiere repository

---
+++ Tema-2/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    //    Tema-2:
+//            -crearea unei aplicații Java.
+//            -crearea a patru variabile cu  valorile de 3, 4, 100, "Nume"
+//            -afișarea acestor variabile în consola din IntelliJ
+    public static void main(String[] args) {
+        int a = 3, b = 4, c = 100;
+        String d = "Nume";
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+        System.out.printf("a=%d,b=%d,c=%d,d=%s",a,b,c,d);
+    }
+}
